6a801a7a64925d513cca3b552e353d919f19f8eb
ragillio <lioajiragil@gmail.com>
2025-03-13 00:41:08 +0700

    part 4

--- 4.23/tests/blog-api.test.js
+++ 4.23/tests/blog-api.test.js
@@ -1,13 +1,15 @@
 const { test, after } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
-const bcrypt = require('bcrypt')
 const app = require('../app')
+
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+
+let token = ''
 
 const api = supertest(app)
 
-let token = ''
 
 test('blogs are returned as json', async () => {
 
